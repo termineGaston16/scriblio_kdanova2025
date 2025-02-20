50ca86ca0e81c4a69fe7049ee7bfa4ee5cdb0a61
kda/nova <gastontermine16@gmail.com>
2025-02-20 19:34:09 -0300

    toolbar dom terminado

--- src/UI/TOOLBAR/Presentation/Components/Toolbar.tsx
+++ src/UI/TOOLBAR/Presentation/Components/Toolbar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { IoSettingsOutline } from "react-icons/io5";
 import { useLocation } from "react-router-dom"
 
 export default function Toolbar() {
@@ -12,6 +13,7 @@ export default function Toolbar() {
     };
 
     const [titleToolbar, setTitleToolbar] = useState<string>(() => formatTitle(location.pathname));
+    const [showSettings, setShowSettings] = useState<boolean>(false)
 
     useEffect(() => {
         setTitleToolbar(formatTitle(location.pathname));
@@ -23,6 +25,17 @@ export default function Toolbar() {
             <a href="" target="_blank" rel="noopener noreferrer">
                 KDA/NOVA 2025
             </a>
+
+            <label htmlFor="">Buscar</label>
+            <input type="search" name="" id="" />
+
+            <button
+                type="button"
+                onClick={() => setShowSettings(true)}
+            >
+                <IoSettingsOutline />
+            </button>
+
 
         </div>
     )
