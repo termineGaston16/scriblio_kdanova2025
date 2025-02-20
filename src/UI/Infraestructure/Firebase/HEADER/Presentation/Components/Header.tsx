7f72517bbaf5a4824561cf6f2e6d138c96c94a27
kda/nova <gastontermine16@gmail.com>
2025-02-19 22:00:10 -0300

    navbar html listo

--- src/UI/Infraestructure/Firebase/HEADER/Presentation/Components/Header.tsx
+++ src/UI/Infraestructure/Firebase/HEADER/Presentation/Components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Navbar from "../../../NAVBAR/Presentation/Components/Navbar";
 
 export default function Header() {
     return (
@@ -8,7 +9,7 @@ export default function Header() {
                 <h1>Scriblio</h1>
             </Link>
 
-
+            <Navbar />
         </header>
     )
 }
