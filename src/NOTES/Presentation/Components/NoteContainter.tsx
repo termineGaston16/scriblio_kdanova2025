31c45b8114dd3ffaa30196f4cb5e1dc7bd6eb9b7
kda/nova <gastontermine16@gmail.com>
2025-02-20 22:27:15 -0300

    notes aun falta

---
+++ src/NOTES/Presentation/Components/NoteContainter.tsx
@@ -0,0 +1,10 @@
+export default function NoteContainer() {
+
+    return (
+        <div>
+            <NoteDetails id={'1'} />
+            <NoteContent id={'1'} />
+            <NoteActions id={'1'} />
+        </div>
+    )
+}
---
+++ src/NOTES/Presentation/Components/NoteDetails.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    id: string
+};
+
+const NoteDetails: React.FC<Props> = ({ id }) => {
+    return (
+        <header>
+            <h1>Título</h1>
+        </header>
+    )
+};
+
+export default NoteDetails; 
