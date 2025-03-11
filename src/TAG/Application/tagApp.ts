7dabb9eac4f3a59c44beb825514b43eaf2729cf6
kda/nova <gastontermine16@gmail.com>
2025-03-11 14:45:44 -0300

    FormToCreateNewTag css terminado

--- src/TAG/Application/tagApp.ts
+++ src/TAG/Application/tagApp.ts
@@ -1,13 +1,13 @@
 export const validateCreateNewTag = (titleTag: string, colorTag: string): string | null => {
 
-    if (typeof titleTag !== 'string' || titleTag.length <= 0) return 'el título no puede estar vacío.';
-    if (typeof colorTag !== 'string' || colorTag.length <= 0) return 'el color no puede estar vacío.';
+    if (typeof titleTag !== 'string' || titleTag.length <= 0) return 'El título NO puede estar vacío.';
+    if (typeof colorTag !== 'string' || colorTag.length <= 0) return 'El color debe seleccionarse.';
 
     const titleFiltred = titleTag
         .trim()
         .replace(/[^\w\s]/g, '');
 
-    if (titleFiltred.length < 3 || titleFiltred.length > 50) return 'el título debe tener entre 3 - 50 caracters';
+    if (titleFiltred.length < 3 || titleFiltred.length > 50) return 'El Título debe tener entre 3 - 50 caracteres.';
 
     return null;
 }
--- src/TAG/Presentation/Components/FormToCreateNewTag.tsx
+++ src/TAG/Presentation/Components/FormToCreateNewTag.tsx
@@ -4,6 +4,8 @@ import { useAppDispatch, useAppSelector } from "../../../UI/Application/Redux/ho
 import { addMessage, cleanMessage } from '../../../UI/Application/Redux/slice/alertMessageSlice'
 import { useAddNewTag } from "../Hooks/useAddNewTag";
 
+import '../Styles/formToCreateNewTag.css'
+
 interface Props {
     closeForm: () => void
 };
@@ -32,6 +34,7 @@ const FormToCreateNewTag: React.FC<Props> = ({ closeForm }) => {
         dispatch(cleanMessage());
 
         mutateUseAddNewTag({ title: titleTag, colorTag: colorTag, id: crypto.randomUUID() as string });
+        closeForm();
     };
 
     useEffect(() => {
@@ -39,51 +42,75 @@ const FormToCreateNewTag: React.FC<Props> = ({ closeForm }) => {
     }, [isSuccessUseAddNewTag])
 
     return (<>
-        <form onSubmit={handleSubmit}>
-            <button
-                type="button"
-                onClick={() => closeForm()}
-            >X</button>
+        <form
+            className="FormToCreateNewTag"
+            onSubmit={handleSubmit}>
+            <div className="FormToCreateNewTag__closeForm__container">
+                <button
+                    className="FormToCreateNewTag__closeForm"
+                    type="button"
+                    onClick={() => closeForm()}
+                >⨉</button>
+            </div>
 
-            <h2>Crear un nuevo Tag</h2>
+            <h2 className="FormToCreateNewTag__titleInfo">Crear nuevo Tag</h2>
 
-            <label htmlFor="nameNewTag">Nombre del Tag: </label>
+            <label
+                className="FormToCreateNewTag__labelNameTag"
+                htmlFor="nameNewTag">Nombre del Tag: </label>
             <input
+                className="FormToCreateNewTag__nameTagInput"
                 type="text"
                 name="nameNewTag"
                 id="nameNewTag"
                 onMouseLeave={() => setMessageInfo(null)}
                 onMouseEnter={() => setMessageInfo(`
-                    *debe contener entre 3 - 50 caracteres.
-                    *no se permiten caracters especiales.
+                    Debe contener entre 3 y 50 caracteres.  
+                    No se permiten caracteres especiales.
                     `)}
             />
 
-            <label htmlFor="colorTag">Asigna un color al Tag</label>
+            <label
+                className="FormToCreateNewTag__labelColorTag"
+                htmlFor="colorTag">Color Representativo:</label>
             <input
+                className="FormToCreateNewTag__colorTagInput"
                 type="color"
                 name="colorTag"
                 id="colorTag"
                 onMouseLeave={() => setMessageInfo(null)}
-                onMouseEnter={() => setMessageInfo(`*asigna un color para representar el Tag. `)}
+                onMouseEnter={() => setMessageInfo(`
+                    El color represantará al Tag. 
+                `)}
             />
 
             {
                 messageAlert &&
-                <p style={{ whiteSpace: "pre-line" }}>(*ERROR: ) {messageAlert}</p>
+                <span
+                    className="FormToCreateNewTag__messageError"
+                >{messageAlert}</span>
             }
             {
                 messageInfo &&
-                <p style={{ whiteSpace: "pre-line" }}>(i) {messageInfo}</p>
+                <p
+                    className="FormToCreateNewTag__messageInfo"
+                >[i] {messageInfo}</p>
             }
-            <button type="submit">Crear</button>
+            <button
+                className="FormToCreateNewTag__btnCreateNewTag"
+                type="submit">CREAR</button>
         </form>
 
         {
             typeof dataUseAddNewTag === 'string' &&
-            <div>
-                {dataUseAddNewTag}
+            <div
+                className="AlertFetch"
+            >
+                <span
+                    className="AlertFetch__text"
+                >{dataUseAddNewTag}</span>
                 <button
+                    className="AlertFetch__close"
                     onClick={() => closeForm()}
                     type="button">Okey</button>
             </div>
